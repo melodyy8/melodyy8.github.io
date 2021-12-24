a26b2d392908e4f14cc787a9094b79e8b3dc99d3
melodyy8 <58495288+melodyy8@users.noreply.github.com>
2021-12-24 12:27:55 -0800

    added indentation

--- contact-form.php
+++ contact-form.php
@@ -24,21 +24,21 @@ if( empty($errors))
 
 {
 
-$to = $myemail;
+   $to = $myemail;
 
-$email_subject = "Contact form submission: $name";
+   $email_subject = "Contact form submission: $name";
 
-$email_body = "You have received a new message. ".
+   $email_body = "You have received a new message. ".
 
-" Here are the details:\n Name: $name \n ".
+   " Here are the details:\n Name: $name \n ".
 
-"Email: $email_address\n Message \n $message";
+   "Email: $email_address\n Message \n $message";
 
-$headers = "From: $myemail\n";
+   $headers = "From: $myemail\n";
 
-$headers .= "Reply-To: $email_address";
+   $headers .= "Reply-To: $email_address";
 
-mail($to,$email_subject,$email_body,$headers);
+   mail($to,$email_subject,$email_body,$headers);
 
 }
 
